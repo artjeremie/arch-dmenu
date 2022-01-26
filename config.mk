42d6efcc0939a546494d21bd7e68fbf45372247c
hoaxdream <hoaxdream@gmail.com>
2022-01-26 14:23:33 +0800

    First commit

--- config.mk
+++ config.mk
@@ -5,13 +5,8 @@ VERSION = 5.0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-# Arch
-#X11INC = /usr/X11R6/include
-#X11LIB = /usr/X11R6/lib
-
-# Void
-X11INC = /usr/include/X11
-X11LIB = /usr/lib/X11
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -20,8 +15,6 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = $(X11INC)/freetype2
 
 # includes and libs
 INCS = -I$(X11INC) -I$(FREETYPEINC)
